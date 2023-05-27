83ffe4e8f3cc4bab1bf30c87cd88717e4d2beba4
Diana <dianagratiana@yahoo.com>
2023-05-28 02:20:00 +0300

    a3

--- a3/a3.c
+++ a3/a3.c
@@ -1,84 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <unistd.h>
 #include <string.h>
 
-#define REQ_PIPE_NAME "REQ PIPE 61586"
-#define RESP_PIPE_NAME "RESP PIPE 61586"
-
-#define CONNECTION_REQUEST_MSG "HELLO"
-#define CONNECTION_SUCCESS_MSG "SUCCESS"
-#define CONNECTION_FAILURE_MSG "ERROR\ncannot create the response pipe | cannot open the request pipe"
+#define RESP_PIPE_NAME "RESP_PIPE_61586"
+#define REQ_PIPE_NAME "REQ_PIPE_61586"
 
 int main() {
-    // Creează pipe-ul de răspuns
+    // Pasul 1: Creează pipe-ul de răspuns
     if (mkfifo(RESP_PIPE_NAME, 0666) == -1) {
-        perror("Cannot create the response pipe");
+        perror("ERROR: cannot create the response pipe");
         exit(1);
     }
 
-    // Deschide pipe-ul de cereri în mod citire
+    // Pasul 2: Deschide pipe-ul de cerere în modul citire
     int reqPipe = open(REQ_PIPE_NAME, O_RDONLY);
     if (reqPipe == -1) {
-        perror("Cannot open the request pipe");
+        perror("ERROR: cannot open the request pipe");
         exit(1);
     }
 
-    // Deschide pipe-ul de răspuns în mod scriere
+    // Pasul 3: Deschide pipe-ul de răspuns în modul scriere
     int respPipe = open(RESP_PIPE_NAME, O_WRONLY);
     if (respPipe == -1) {
-        perror("Cannot open the response pipe");
+        perror("ERROR: cannot open the response pipe");
         exit(1);
     }
 
-    // Trimite mesajul de cerere de conexiune
-    if (write(respPipe, CONNECTION_REQUEST_MSG, strlen(CONNECTION_REQUEST_MSG)) == -1) {
-        perror("Failed to write connection request");
+    // Pasul 4: Scrie mesajul de cerere în pipe-ul de răspuns
+    const char *requestMessage = "HELLO!";
+    if (write(respPipe, requestMessage, strlen(requestMessage) + 1) == -1) {
+        perror("ERROR: cannot write to the response pipe");
+        exit(1);
+    }
+printf("SUCCES\n");
+
+    // Citeste raspunsul din pipe-ul de cerere
+    char responseMessage[256];
+    ssize_t bytesRead = read(reqPipe, responseMessage, sizeof(responseMessage));
+    if (bytesRead == -1) {
+        perror("ERROR: cannot read from the request pipe");
         exit(1);
     }
 
-    // Primește mesajul de răspuns
-    char response[1024];
-    if (read(reqPipe, response, sizeof(response)) == -1) {
-        perror("Failed to read connection response");
-        exit(1);
+    // Verifică dacă s-a primit răspunsul complet
+    if (bytesRead > 0) {
+        responseMessage[bytesRead] = '\0';  // Asigură terminarea șirului de caractere
+
+        // Afiseaza raspunsul
+        printf("%s\n", responseMessage);
     }
 
-    // Verifică mesajul de răspuns și afișează rezultatul corespunzător
-    if (strcmp(response, CONNECTION_SUCCESS_MSG) == 0) {
-        printf("Connection Successful Message\n");
-        printf("%s\n", response);
-    } else {
-        printf("Connection Failure Message\n");
-        printf("%s\n", CONNECTION_FAILURE_MSG);
-    }
 
-    // Buclă principală
-    while (1) {
-        // Citește cererea din pipe-ul de cereri
-        char request[1024];
-        if (read(reqPipe, request, sizeof(request)) == -1) {
-            perror("Failed to read request");
-            exit(1);
-        }
-
-        // Tratează cererea și obține rezultatul
-        // (aici ar trebui adăugat codul pentru tratarea diferitelor tipuri de cereri)
-
-        // Scrie rezultatul în pipe-ul de răspuns
-        if (write(respPipe, response, strlen(response)) == -1) {
-            perror("Failed to write response");
-            exit(1);
-        }
-    }
-
-    // Închide pipe-urile și eliberează resursele
+    // Închide pipe-urile
     close(reqPipe);
     close(respPipe);
-    unlink(RESP_PIPE_NAME);
 
     return 0;
 }
